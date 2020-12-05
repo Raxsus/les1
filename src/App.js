7a4b5a4afe7b2147b360f3d8a4cfc4b1c47d03f7
Pavel Sambindalov <raxsus@gmail.com>
2020-12-05 13:16:59 +0300

    14. Добавление событий

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ class App extends Component {
     cars: [
       { name: 'Ford', year: 2018 },
       { name: 'Audi', year: 2015 },
-      { name: 'Mazda', year: 2019 }
+      { name: 'Mazda', year: 2019 },
+      { name: 'Жигули', year: 1985 }
     ],
     pageTitle: 'React Components'
+  }
+
+  changeTitleHandler = () => {
+    console.log('click');
   }
 
   render() {
@@ -22,9 +27,12 @@ class App extends Component {
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
+        <button onClick={this.changeTitleHandler}>Change Title</button>
+
         <Car name={cars[0].name} year={cars[0].year} />
         <Car name={cars[1].name} year={cars[1].year} />
         <Car name={cars[2].name} year={cars[2].year} />
+        <Car name={cars[3].name} year={cars[3].year} />
       </div>
     );
   }
