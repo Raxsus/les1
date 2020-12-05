11e7d3fae7e2a2b1bd0fb71597a553c28d2f0296
Pavel Sambindalov <raxsus@gmail.com>
2020-12-05 13:35:30 +0300

    16. Передача параметров в функцию

--- src/App.js
+++ src/App.js
@@ -14,20 +14,15 @@ class App extends Component {
     pageTitle: 'React Components'
   }
 
-  changeTitleHandler = () => {
-
-    const _old = this.state.pageTitle;
-
-    const _new = _old + ' (changed)';
+  changeTitleHandler = (newTitle) => {
 
     this.setState({
-      pageTitle: _new
+      pageTitle: newTitle
     });
 
   }
 
   render() {
-    console.log('render');
     const cars = this.state.cars;
 
     const divStyle = {textAlign: 'center'};
@@ -35,12 +30,24 @@ class App extends Component {
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
-        <button onClick={this.changeTitleHandler}>Change Title</button>
+        {/* Better to use .bind */}
+        <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>
+          Change Title
+        </button>
 
-        <Car name={cars[0].name} year={cars[0].year} />
-        <Car name={cars[1].name} year={cars[1].year} />
-        <Car name={cars[2].name} year={cars[2].year} />
-        <Car name={cars[3].name} year={cars[3].year} />
+        <Car
+          name={cars[0].name}
+          year={cars[0].year}
+          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
+        />
+        <Car
+          name={cars[1].name}
+          year={cars[1].year}
+          onChangeTitle={() => this.changeTitleHandler(cars[1].name)} />
+        <Car
+          name={cars[2].name}
+          year={cars[2].year}
+          onChangeTitle={() => this.changeTitleHandler(cars[2].name)} />
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default ({name, year}) => (
+export default ({name, year, onChangeTitle}) => (
   <div>
     <h3>Car Name: {name}</h3>
     <p>Year: <strong>{year}</strong></p>
+    <button onClick={onChangeTitle}>Click</button>
   </div>
 );
